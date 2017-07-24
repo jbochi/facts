bdf871faca2a6576ec44bf3b3876b1689d5fec09
Juarez Bochi <jbochi@gmail.com>
2017-07-24 15:04:09 -0400

    Initial commit

---
+++ vectormodel/vector_model.go
@@ -0,0 +1,182 @@
+package vectormodel
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+
+	"github.com/gonum/matrix/mat64"
+)
+
+type (
+	// VectorModel is a struct to handle document vector space models
+	// We assume that the document vectors were calculated according to the paper
+	// "Collaborative Filtering for Implicit Feedback Datasets".
+	// Given an user history, we compute user vector and recommendations online.
+	VectorModel struct {
+		confidence            float64
+		regularization        float64
+		docIDs                []int
+		docIndexes            map[int]int
+		nFactors              int
+		itemFactorsY          *mat64.Dense
+		squaredItemFactorsYtY *mat64.Dense
+	}
+
+	// DocumentScore is the result of a recommendation
+	DocumentScore struct {
+		DocumentID int
+		Score      float64
+	}
+)
+
+func (a byDocScoreDesc) Len() int           { return len(a) }
+func (a byDocScoreDesc) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byDocScoreDesc) Less(i, j int) bool { return a[i].Score > a[j].Score }
+
+type byDocScoreDesc []DocumentScore
+
+// NewVectorModel creates a new VectorModel
+func NewVectorModel(documents map[int][]float64, confidence, regularization float64) (*VectorModel, error) {
+	var vm VectorModel
+	vm.confidence = confidence
+	vm.regularization = regularization
+	vm.docIDs = make([]int, len(documents))
+	vm.docIndexes = make(map[int]int)
+
+	data := make([]float64, 0)
+	i := 0
+	for doc, vector := range documents {
+		if i == 0 {
+			vm.nFactors = len(vector)
+		} else if len(vector) != vm.nFactors {
+			return nil, errors.New("Invalid vector size")
+		}
+		vm.docIndexes[doc] = i
+		vm.docIDs[i] = doc
+		data = append(data, vector...)
+		i++
+	}
+	vm.itemFactorsY = mat64.NewDense(len(documents), vm.nFactors, data)
+
+	var YtY mat64.Dense
+	YtY.Mul(vm.itemFactorsY.T(), vm.itemFactorsY)
+	vm.squaredItemFactorsYtY = &YtY
+
+	return &vm, nil
+}
+
+// Rank sorts a list of candidate assets for a given user history
+func (vm *VectorModel) Rank(candidates *[]int, seenDocs *map[int]bool) (err error) {
+	candidateScores, err := vm.scoreCandidates(candidates, seenDocs)
+	if err != nil {
+		return err
+	}
+	for i, candidateScore := range candidateScores {
+		(*candidates)[i] = candidateScore.DocumentID
+	}
+	return nil
+}
+
+// Recommend returns a list of recommendedDocs and a list of scores
+func (vm *VectorModel) Recommend(seenDocs *map[int]bool, n int) (recommendations []DocumentScore, err error) {
+	recommendations, err = vm.scoreCandidates(&vm.docIDs, seenDocs)
+	if err != nil {
+		return nil, err
+	}
+	if len(recommendations) > n {
+		recommendations = recommendations[:n]
+	}
+	return recommendations, nil
+}
+
+func (vm *VectorModel) scoreCandidates(candidates *[]int, seenDocs *map[int]bool) (recommendations []DocumentScore, err error) {
+	confidenceMap := vm.confidenceMap(seenDocs)
+	if len(confidenceMap) == 0 {
+		return nil, fmt.Errorf("No seen doc is in model. History: %d Model: %d",
+			len(*seenDocs), len(vm.docIndexes))
+	}
+	userVec, err := vm.userVector(confidenceMap)
+	if err != nil {
+		return recommendations, err
+	}
+	scoresVec := vm.scoresForUserVec(&userVec)
+	candidateScores := make([]DocumentScore, len(*candidates))
+	for i, doc := range *candidates {
+		var score float64
+		if _, docAlreadySeen := (*seenDocs)[doc]; docAlreadySeen {
+			score = -1
+		} else if docIndex, docInModel := vm.docIndexes[doc]; !docInModel {
+			score = 0
+		} else {
+			score = scoresVec.At(docIndex, 0)
+		}
+		candidateScores[i] = DocumentScore{doc, score}
+	}
+	sort.Sort(byDocScoreDesc(candidateScores))
+	return candidateScores, nil
+}
+
+func (vm *VectorModel) confidenceMap(seenDocs *map[int]bool) map[int]float64 {
+	confidenceMap := make(map[int]float64)
+	for doc := range *seenDocs {
+		if _, inModel := vm.docIndexes[doc]; inModel {
+			confidenceMap[doc] = vm.confidence
+		}
+	}
+	return confidenceMap
+}
+
+// userVector returns the user vector for a given set of consumed documents
+func (vm *VectorModel) userVector(confidenceMap map[int]float64) (mat64.Vector, error) {
+	// We follow the notation from the paper "Collaborative Filtering for Implicit Feedback Datasets"
+	// Please see github.com/benfred/implicit as a reference implementation
+
+	// We solve the following linear equation:
+	// Xu = (YtCuY + regularization*I)i^-1 * YtYCuPu
+
+	// A = YtCuY + reg * I = YtY + reg * I + Yt(Cu - I)Y
+	// We initialize A to YtY + reg * I  and sum the last term for each doc
+	var A mat64.Dense
+	A.Add(vm.squaredItemFactorsYtY, eye(vm.nFactors, vm.regularization))
+
+	// b = YtCuPu
+	b := mat64.NewVector(vm.nFactors, make([]float64, vm.nFactors))
+
+	for doc, confidence := range confidenceMap {
+		index, docFound := vm.docIndexes[doc]
+		if !docFound {
+			continue
+		}
+		factor := vm.itemFactorsY.RowView(index)
+
+		// A += (confidence - 1) * np.outer(factor, factor)
+		var factor2 mat64.Dense
+		factor2.Mul(factor, factor.T())
+		factor2.Scale(confidence-1, &factor2)
+		A.Add(&A, &factor2)
+
+		// b += confidence * factor
+		b.AddScaledVec(b, confidence, factor)
+	}
+
+	var x mat64.Vector
+	err := x.SolveVec(&A, b)
+	return x, err
+}
+
+// scoresForUserVec returns a vector with scores given set of consumed documents
+func (vm *VectorModel) scoresForUserVec(userVec *mat64.Vector) mat64.Vector {
+	var y mat64.Vector
+	y.MulVec(vm.itemFactorsY, userVec)
+	return y
+}
+
+// eye returns an identity matrix with size n and `value` in the diagonal
+func eye(n int, value float64) mat64.Matrix {
+	m := mat64.NewDense(n, n, make([]float64, n*n))
+	for i := 0; i < n; i++ {
+		m.Set(i, i, value)
+	}
+	return m
+}
---
+++ vectormodel/vector_model_test.go
@@ -0,0 +1,231 @@
+package vectormodel
+
+import (
+	"math"
+	"testing"
+
+	"github.com/gonum/matrix/mat64"
+)
+
+func TestVectorModelConstructor(t *testing.T) {
+	confidence := 1.0
+	regularization := 0.01
+	docs := make(map[int][]float64)
+	docs[1234] = []float64{1, 2, 3}
+	vm, err := NewVectorModel(docs, confidence, regularization)
+
+	if err != nil {
+		t.Fatalf("Failed to create vector model %s", err)
+	}
+
+	if vm.nFactors != 3 {
+		t.Errorf("Expecting 3 factors, got %d", vm.nFactors)
+	}
+	if vm.confidence != confidence {
+		t.Errorf("Wrong confidence: %f", vm.confidence)
+	}
+	if vm.regularization != regularization {
+		t.Errorf("Wrong regularization: %f", vm.regularization)
+	}
+
+	Y := vm.itemFactorsY
+	YtY := vm.squaredItemFactorsYtY
+	Yrows, Ycols := Y.Dims()
+	if Yrows != 1 || Ycols != 3 {
+		t.Errorf("Y has wrong dimensions (%d, %d) instead of (3, 1)", Yrows, Ycols)
+	}
+
+	a, b, c := Y.At(0, 0), Y.At(0, 1), Y.At(0, 2)
+	if a != 1 || b != 2 || c != 3 {
+		t.Errorf("Y has the wrong values %f, %f, %f", a, b, c)
+	}
+
+	YtYrows, YtYcols := YtY.Dims()
+	if YtYrows != 3 || YtYcols != 3 {
+		t.Errorf("YtY has wrong dimensions (%d, %d) instead of (3, 3)",
+			YtYrows, YtYcols)
+	}
+
+	a, b, c = YtY.At(0, 0), YtY.At(0, 1), YtY.At(0, 2)
+	if a != 1 || b != 2 || c != 3 {
+		t.Errorf("first row of YtY has the wrong values: %f, %f, %f", a, b, c)
+	}
+	a, b, c = YtY.At(1, 0), YtY.At(1, 1), YtY.At(1, 2)
+	if a != 2 || b != 4 || c != 6 {
+		t.Errorf("second row of YtY has the wrong values: %f, %f, %f", a, b, c)
+	}
+	a, b, c = YtY.At(2, 0), YtY.At(2, 1), YtY.At(2, 2)
+	if a != 3 || b != 6 || c != 9 {
+		t.Errorf("third row of YtY has the wrong values: %f, %f, %f", a, b, c)
+	}
+
+}
+
+func TestVectorModelConstructorWithInvalidVectors(t *testing.T) {
+	docs := make(map[int][]float64)
+	docs[1234] = []float64{1, 2, 3}
+	docs[1235] = []float64{1, 2, 3, 5}
+	_, err := NewVectorModel(docs, 1.0, 0.01)
+
+	if err == nil {
+		t.Fatalf("Should not allow vectors with different sizes")
+	}
+}
+
+func TestUserVector(t *testing.T) {
+	defaultConfidence := 40.0
+	regularization := 0.01
+	docs := make(map[int][]float64)
+	docs[1234] = []float64{1, 2, 3}
+	vm, err := NewVectorModel(docs, defaultConfidence, regularization)
+	if err != nil {
+		t.Fatalf("Failed to create vector model %s", err)
+	}
+
+	confidence := map[int]float64{1234: 40.0, 666: 1.0}
+
+	user, err := vm.userVector(confidence)
+
+	if err != nil {
+		t.Fatalf("Error solving user vector: %s", err)
+	}
+
+	rows, cols := user.Dims()
+	if rows != 3 || cols != 1 {
+		t.Fatalf("Invalid user vec dimensions: %d, %d", rows, cols)
+	}
+
+	a, b, c := user.At(0, 0), user.At(1, 0), user.At(2, 0)
+	if math.Abs(a-0.0714273)+math.Abs(b-0.14285459)+math.Abs(c-0.21428189) > 1e-4 {
+		t.Fatalf("Invalid user vec: [%f, %f, %f]", a, b, c)
+	}
+}
+
+func TestScoresForUserVec(t *testing.T) {
+	regularization := 0.01
+	confidence := 40.0
+	docs := make(map[int][]float64)
+	docs[1234] = []float64{1, 2, 3}
+	docs[4567] = []float64{3, 2, 1}
+	vm, err := NewVectorModel(docs, confidence, regularization)
+	if err != nil {
+		t.Fatalf("Failed to create vector model %s", err)
+	}
+
+	userVec := mat64.NewVector(3, []float64{0.2, 0.1, 0.0})
+	scores := vm.scoresForUserVec(userVec)
+
+	rows, cols := scores.Dims()
+	if rows != 2 || cols != 1 {
+		t.Fatalf("Invalid scores dimensions: %d, %d", rows, cols)
+	}
+
+	score1 := scores.At(vm.docIndexes[1234], 0)
+	score2 := scores.At(vm.docIndexes[4567], 0)
+	if score1 != (0.2*1+0.1*2) || score2 != (3*0.2+2*0.1) {
+		t.Fatalf("Invalid scores: %f (%d), %f (%d)", score1, vm.docIndexes[1234], score2, vm.docIndexes[4567])
+	}
+}
+
+func TestRecommend(t *testing.T) {
+	confidence := 40.0
+	regularization := 0.01
+	docs := make(map[int][]float64)
+	docs[1234] = []float64{1, 2, 3}
+	docs[4567] = []float64{3, 2, 1}
+	vm, err := NewVectorModel(docs, confidence, regularization)
+	if err != nil {
+		t.Fatalf("Failed to create vector model %s", err)
+	}
+
+	seenDocs := map[int]bool{1234: true}
+	n := 10
+
+	recommendations, err := vm.Recommend(&seenDocs, n)
+	if err != nil {
+		t.Fatalf("Failed to recommend %s", err)
+	}
+	if len(recommendations) != 2 {
+		t.Fatalf("Wrong number of recommendations: %v", recommendations)
+	}
+	if recommendations[0].DocumentID != 4567 {
+		t.Errorf("Wrong recommendation: %v", recommendations[0])
+	}
+	if recommendations[1].DocumentID != 1234 {
+		t.Errorf("Wrong recommendation: %v", recommendations[1])
+	}
+
+	// This is how you can obtain the excpected scores in python:
+	//
+	// import numpy as np
+	// Y = np.array([[1, 2, 3], [3, 2, 1]])
+	// YtY = Y.T.dot(Y)
+	// YtY
+	// regularization = 0.01
+	// confidence = 40
+	// A = YtY + regularization * np.eye(3)
+	// b = np.zeros(3)
+	// factor = Y[0]
+	// A += (confidence - 1) * np.outer(factor, factor)
+	// b += confidence * factor
+	// user = np.linalg.solve(A, b)
+	// np.dot(Y, user)
+
+	if math.Abs(recommendations[0].Score-0.00104011) > 1e-5 {
+		t.Errorf("Wrong score: %f", recommendations[0].Score)
+	}
+}
+
+func TestRecommendReturnsTopItems(t *testing.T) {
+	confidence := 40.0
+	regularization := 0.01
+	docs := make(map[int][]float64)
+	docs[0] = []float64{1, 2, 3}
+	docs[1] = []float64{1, 2, 3.01}
+	docs[2] = []float64{1, 2, 3.02}
+	docs[3] = []float64{3, 2, 1}
+	docs[4] = []float64{1, 2, 3.03}
+	vm, err := NewVectorModel(docs, confidence, regularization)
+	if err != nil {
+		t.Fatalf("Failed to create vector model %s", err)
+	}
+
+	seenDocs := map[int]bool{0: true}
+	n := 3
+	recs, err := vm.Recommend(&seenDocs, n)
+	if err != nil {
+		t.Fatalf("Failed to recommend %s", err)
+	}
+	if len(recs) != 3 {
+		t.Fatalf("Wrong number of recommendations: %v", recs)
+	}
+	if recs[0].DocumentID != 1 || recs[1].DocumentID != 2 || recs[2].DocumentID != 4 {
+		t.Errorf("Wrong recommendations: %v", recs)
+	}
+}
+
+func TestRankSortsTopItems(t *testing.T) {
+	confidence := 40.0
+	regularization := 0.01
+	docs := make(map[int][]float64)
+	docs[0] = []float64{1, 2, 3}
+	docs[1] = []float64{1, 2, 3}
+	docs[2] = []float64{1, 2, 3}
+	docs[3] = []float64{3, 2, 1}
+	docs[4] = []float64{1, 2, 3}
+	vm, err := NewVectorModel(docs, confidence, regularization)
+	if err != nil {
+		t.Fatalf("Failed to create vector model %s", err)
+	}
+
+	seenDocs := map[int]bool{0: true}
+	items := []int{0, 1, 3, 10}
+	err = vm.Rank(&items, &seenDocs)
+	if err != nil {
+		t.Fatalf("Failed to recommend %s", err)
+	}
+	// Order is: Most similar, Not read, Unknown, read
+	if items[0] != 1 || items[1] != 3 || items[2] != 10 || items[3] != 0 {
+		t.Errorf("Wrong recommendations: %v", items)
+	}
+}
